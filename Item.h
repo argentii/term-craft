5cf474aaa1f498564a08c9bd287632e5d77dc90b
argentii <gitgud07d4@gmail.com>
2025-12-29 23:45:48 -0500

    Initial commit

---
+++ Item.h
@@ -0,0 +1,16 @@
+//Name: Item.cpp
+//Author: Argentii
+//Desc: Header file for item class
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Item {
+  int id;
+  std::string name;
+  int maxStackSize;
+  bool isDamageable;
+  
+};
